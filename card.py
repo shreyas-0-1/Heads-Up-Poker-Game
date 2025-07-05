7ce5166a501a2d685d1628adcb705b0822aad35a
Shreyas <shreyasnsingh@gmail.com>
2025-07-05 16:59:01 -0400

    Finished get/checkStraight

--- card.py
+++ card.py
@@ -1,3 +1,5 @@
+RANK_ORDER = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+
 class Card:
     def __init__(self, rank:str, suit:str):
         self.rank = rank
@@ -14,5 +16,6 @@ class Card:
     
     def __hash__(self):
         return hash((self.rank, self.suit))
-
-
+    
+    def __lt__(self, other):
+        return RANK_ORDER[self.rank] < RANK_ORDER[other.rank]
--- hand.py
+++ hand.py
@@ -1,24 +1,37 @@
 from card import Card
 
+#dicts that map rank to its value and suit to number
 RANK_ORDER = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
 SUIT_ORDER = {'♣️':1, '♠️':2, '♥️':3, '♦️':4}
+
+#inverses of above dicts
+INVERSE_RANK_ORDER = dict()
+for i in range(13):
+    INVERSE_RANK_ORDER[list(RANK_ORDER.values())[i]] = list(RANK_ORDER.keys())[i]
+
 INVERSE_SUIT_ORDER = {1:'♣️', 2:'♠️', 3:'♥️', 4:'♦️'}
 
 class Hand:
-    #only for 5 card hands rn
-
-    def __init__(self, myHand=list):
-        self.myHand = myHand
-        self.bestHand = None
+    def __init__(self, playerHand=list):
+        self.playerHand = playerHand
+        #self.bestHand = None
         self.handRanks = list()
         self.handSuits = list()
 
-        for card in myHand:
+        for i in range(len(self.playerHand)):
+            for k in range(i + 1, len(self.playerHand)):
+                if self.playerHand[i] < self.playerHand[k]:
+                    self.playerHand[i], self.playerHand[k] = self.playerHand[k], self.playerHand[i]
+
+        #<handRanks> lists player's cards' ranks in descending order
+        for card in playerHand:
             self.handRanks.append(RANK_ORDER[card.rank])
 
-        self.handRanks.sort()
+        self.handRanks.sort(reverse=True)
 
-        for card in myHand:
+
+        #<handSuits> lists player's cards' suits in order c, s, h, d
+        for card in playerHand:
             self.handSuits.append(SUIT_ORDER[card.suit])
 
         self.handSuits.sort()
@@ -34,18 +47,27 @@ class Hand:
     
     def getFlush(self):
         suit = self.handSuits[3]
-        highRank = 0
+        bestFlush = list()
 
-        for card in self.myHand:
-            if card.suit == suit and RANK_ORDER[card.rank] > highRank:
-                highRank = RANK_ORDER[card.rank]
+        for card in self.playerHand:
+            if card.suit == suit:
+                bestFlush.append(card)
+
+        for i in range(len(bestFlush)):
+            for k in range(i + 1, len(bestFlush)):
+                if bestFlush[i] < bestFlush[k]:
+                    bestFlush[i], bestFlush[k] = bestFlush[k], bestFlush[i]
+
+        while len(bestFlush) > 5:
+            bestFlush.pop(len(bestFlush) - 1)
         
-        return ('flush', highRank)
+        return ('flush', bestFlush)
                 
     
     def checkStraight(self):
         noCopies = self.handRanks.copy()
         noCopies = sorted(list(set(noCopies)))
+        print(noCopies)
 
         for i in range(0, len(noCopies)-4):
             temp = noCopies[i:i+5]
@@ -56,25 +78,26 @@ class Hand:
         return False
     
     def getStraight(self):
-        highRank = 0
-        noCopies = self.handRanks.copy()
-        noCopies = sorted(list(set(noCopies)))
+        copy = self.playerHand.copy()
         
-        for i in range(len(noCopies)-1, 3, -1): 
-            temp = noCopies[i-4:i+1]
-
-            if temp[4] - temp[0] == 4:
-                highRank = temp[4]
+        i = 0
+        while i < len(copy):
+            k = i + 1
+            while k < len(copy):
+                if copy[i].rank == copy[k].rank:
+                    copy.pop(k)
+                    k = k - 1
+                k = k + 1
+            
+            i = i + 1
+        
+        bestStraight = list()
+        for j in range(len(copy) - 4):
+            if RANK_ORDER[copy[j].rank] - RANK_ORDER[copy[j + 4].rank] == 4:
+                bestStraight = copy[j:j + 5]
                 break
-        
-        return ('straight', highRank)
-    
-    def checkQuads(self):
-        for value in RANK_ORDER.values():
-            if self.handRanks.count(value) == 4:
-                return True
-        
-        return False
+
+        return ('straight', bestStraight)
     
     def getQuads(self):
         highRank = self.handRanks[3]
@@ -163,6 +186,8 @@ class Hand:
             
         return False'''
 
-'''cards = [Card('3', '♣️'), Card('2', '♣️'), Card('4', '♣️'), Card('5', '♣️'), Card('A', '♣️'), Card('4', '♣️'), Card('6', '♣️')]
-hand = Hand(cards)'''
+cards = [Card('A', '♣️'), Card('T', '♣️'), Card('J', '♣️'), Card('K', '♦️'), Card('3', '♦️'), Card('4', '♣️'), Card('Q', '♦️')]
+hand = Hand(cards)
+print(hand.playerHand)
+print(hand.getStraight())
 
