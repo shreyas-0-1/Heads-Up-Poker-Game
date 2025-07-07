f7ce9bd1f401844839a2d83197c87c374c7843e7
Shreyas <shreyasnsingh@gmail.com>
2025-07-07 18:33:14 -0400

    Modded deck.py to improve dealing+burning and to use one Deck obj for entire game

--- deck.py
+++ deck.py
@@ -3,22 +3,33 @@ import random as rand
 
 RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
 SUITS = ('♣️', '♠️', '♥️', '♦️')
+CARDS_LIST = list()
+
+for rank in RANKS:
+    for suit in SUITS:
+        CARDS_LIST.append(Card(rank, suit))
 
 class Deck:
     def __init__(self):
-        self.deck = list()
+        self.cards = dict()
 
-        for rank in RANKS:
-            for suit in SUITS:
-                self.deck.append(Card(rank, suit))
+    def shuffle(self):
+        rand.shuffle(CARDS_LIST)
+
+        for card in CARDS_LIST:
+            self.cards[card] = 1
 
     def display(self):
         temp = ""
 
-        for card in self.deck:
-            temp = temp + f"{card}, "
+        for card in self.cards:
+            if self.cards[card] != 0:
+                temp = temp + f"{card}, "
 
         print(temp[:-2])
 
-    def shuffle(self):
-        rand.shuffle(self.deck)
+    def pop(self):
+        for card in self.cards:
+            if self.cards[card] != 0:
+                self.cards.update({card: 0})
+                return card
